bf3e89d0731340a080fc4ebf65709f453ccaf012
Pete Haddrell <petejhaddrell@gmail.com>
2016-05-29 18:28:56 +0100

    url weather calls are now being made

--- Source/Weather.cpp
+++ Source/Weather.cpp
@@ -22,19 +22,28 @@ Weather::~Weather()
 
 void Weather::connect()
 {
-    latitude = location.getCoordinate().x;
-    longitude = location.getCoordinate().y;
+   //off while permissions get sorted
+//    latitude = location.getCoordinate().x;
+//    longitude = location.getCoordinate().y;
+    
+    //hard set to london
+    latitude = 51.5074;
+    longitude = 0.1278;
     
     String lat (latitude);
     String longit (longitude);
     
     //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
-    String urlStr ("api.openweathermap.org/");
-    urlStr += key.getKey();
-    urlStr += "/2.5/weather?lat=";
+    String urlStr = "http://api.openweathermap.org/data/2.5/weather?lat=";
     urlStr += lat;
     urlStr += "&lon=";
     urlStr += longit;
+    urlStr += "&APPID=";
+    urlStr += key.getKey();
     
-//    std::cout << urlStr << "\n";
+    std::cout << urlStr << "\n";
+    
+    URL url (urlStr);
+    String textStream = juce::JSON::toString (url.readEntireTextStream(), true);
+    std::cout << textStream << std::endl;
 }
